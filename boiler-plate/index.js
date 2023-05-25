43f7ab38f04ddc0578a7b035f63d15bc6dfecc7d
sjy2335 <106982330+sjy2335@users.noreply.github.com>
2023-05-25 14:31:40 +0900

    week5

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://joey1109:joey2335!!@boilerplate.1iqzsbn.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://joey1109:joey2335!!@boilerplate.1iqzsbn.mongodb.net/?retryWrites=true&w=majority', {
+   useNewUrlParser: true, useUnifiedTopology: true
+})
+//MongoParseError: options usecreateindex, usefindandmodify are not supported
 .then(()=>console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
